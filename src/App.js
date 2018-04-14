acea8e400dc387f8296f2b8ef8e3a9d1999bceeb
workvictor <workvictor@yandex.ru>
2018-04-14 21:54:53 +0300

    update App demo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import React        from 'react';
-import styled       from 'styled-components';
-import { Calendar } from './components';
+import React  from 'react';
+import styled from 'styled-components';
+import {
+  Calendar,
+  calendarTypes,
+  CheckBox
+}             from './components';
 
 
+const Flex=styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ChecksWrapper=styled(Flex)`
+  margin-bottom: 16px;
+`;
+
+const StyledCheckBox=styled(CheckBox)`
+  margin: 0 8px;
+`;
 const Wrapper=styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
   height: 100%;
   width: 100%;
   position: fixed;
 `;
 
 export class App extends React.Component{
-  render(){
-    return (
-      <Wrapper>
-        <Calendar
+  constructor(){
+    super();
+    this.state={
+      selected: calendarTypes.datePicker
+    };
+  }
 
+  onDatePickerSelected=()=>{
+    this.setState({ selected: calendarTypes.datePicker });
+  };
+  onRangePickerSelected=()=>{
+    this.setState({ selected: calendarTypes.rangePicker });
+  };
+  onTimePickerSelected=()=>{
+    this.setState({ selected: calendarTypes.timePicker });
+  };
+
+  render(){
+    const { className }=this.props;
+    const { selected }=this.state;
+    return (
+      <Wrapper className={className}>
+        <ChecksWrapper>
+          <StyledCheckBox selected={selected === calendarTypes.datePicker} onSelect={this.onDatePickerSelected}>
+            date picker
+          </StyledCheckBox>
+          <StyledCheckBox selected={selected === calendarTypes.rangePicker} onSelect={this.onRangePickerSelected}>
+            range picker
+          </StyledCheckBox>
+          <StyledCheckBox selected={selected === calendarTypes.timePicker} onSelect={this.onTimePickerSelected}>
+            time picker
+          </StyledCheckBox>
+        </ChecksWrapper>
+        <Calendar
+          calendarType={selected}
         />
       </Wrapper>
     );
